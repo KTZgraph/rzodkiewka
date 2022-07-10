43d7fd7839bb7a2cdda87d03049d4a8d82b7d4dc
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-07-10 10:44:45 +0200

    init files changed

--- rzodkiewka/__init__.py
+++ rzodkiewka/__init__.py
@@ -1,7 +1,6 @@
-from cwe.downloader import CWEDownloader
-from cwe.simplifier import CWESimplifier
-
-from cve.downloader import CVEDownloader
-from cve.simplifier import CVESimplifier
-from cve.splitter import CVESplitter
-from cve.cve_list import CVEList
+from .cwe.downloader import CWEDownloader
+from .cwe.simplifier import CWESimplifier
+from .cve.downloader import CVEDownloader
+from .cve.simplifier import CVESimplifier
+from .cve.splitter import CVESplitter
+from .cve.cve_list import CVEList
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'A basic package for CWEs, CVEs download and safe simplified files.'
 
 # Setting up
