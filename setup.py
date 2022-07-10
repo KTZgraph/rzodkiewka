b2d165d8ef6bf74c3a4b3089c913db33d3a9ce26
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-07-10 10:10:40 +0200

    setup version 0.0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'A basic package for CWEs, CVEs download and safe simplified files.'
 
 # Setting up
