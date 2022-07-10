aad2865a354438087d29da02017a26af93295794
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-07-10 10:10:01 +0200

    import __init__ changed

--- rzodkiewka/__init__.py
+++ rzodkiewka/__init__.py
@@ -1,1 +1,7 @@
-from main import main
+from cwe.downloader import CWEDownloader
+from cwe.simplifier import CWESimplifier
+
+from cve.downloader import CVEDownloader
+from cve.simplifier import CVESimplifier
+from cve.splitter import CVESplitter
+from cve.cve_list import CVEList
